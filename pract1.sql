652a397e086d3d7efa5b0a9207b9013b3f90b4c4
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-03-23 16:46:44 +0530

    1 Ques

---
+++ pract1.sql
@@ -0,0 +1,9 @@
+-- Ques :- Create a database for your comapny named XYZ
+--Step1:- create a table inside the DB to store employee info (id, name and salary).
+--Step2:- Add following info in DB
+-- 1, "adam", 25000
+-- 2, "bob", 30000
+-- 3, "caset", 40000
+--Step3:- Select and view all your table data.
+
+
