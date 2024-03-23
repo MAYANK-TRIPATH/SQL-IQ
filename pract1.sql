28b6986a3fecfb2e4194ca54bf1fcda3ae9775b8
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-03-23 16:53:49 +0530

    1 sol

--- pract1.sql
+++ pract1.sql
@@ -6,4 +6,20 @@
 -- 3, "caset", 40000
 --Step3:- Select and view all your table data.
 
+CREATE DATABASE xyz_company;
+USE xyz_company;
 
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    salary INT
+);
+
+INSERT INTO employee
+(id, name, salary)
+VALUES
+(1, "adam", 25000),
+(2, "adam", 35000),
+(3, "adam", 45000);
+ 
+SELECT * FROM employee; 
